20886a2f3d4e60c3805a514f63fbb8c37dd00961
Nvphuc18i <48077303+Nvphuc18i@users.noreply.github.com>
2019-02-28 10:40:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaoLopTinhTong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bài 1
+            tinh_tong tong = new tinh_tong();
+            tong.nhapn();
+            tong.tinhTong();
+            tong.inTong();
+            tong.tinhTong2();
+            tong.tinhTong3();
+            tong.tongUoc();
+
+
+            //Bài 2
+            TinhTau tau= new TinhTau();
+            tau.nhapMaDoanTau();
+            tau.tinhTien();
+
+            tau.inMatau();
+            Console.ReadLine();
+
+        }
+    }
+}
