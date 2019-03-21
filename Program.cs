173e43acbc83b67ecc23f72c5026de74cbea1c30
Nvphuc18i <48077303+Nvphuc18i@users.noreply.github.com>
2019-03-21 16:25:24 +0700

    bài tập ngày 21.3

--- Program.cs
+++ Program.cs
@@ -4,29 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaoLopTinhTong
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Bài 1
-            tinh_tong tong = new tinh_tong();
-            tong.nhapn();
-            tong.tinhTong();
-            tong.inTong();
-            tong.tinhTong2();
-            tong.tinhTong3();
-            tong.tongUoc();
+            int n;
+            Console.WriteLine("Nhap so sv:");
+            n = Int32.Parse(Console.ReadLine());
+            //tao mang doi tuong sv
+            sinhvien[] sv = new sinhvien[n];
+            for (int i = 0; i < n; i++)
+            {
+                sv[i] = new sinhvien();
+                sv[i].Nhap();
+                sv[i].In();
+            }
+            //tim ra sv nao co diem tb thap nhat
+            float min = sv[0].tinhTB();
+            for (int i = 1; i < n; i++)
+            {
+                if (sv[i].tinhTB() < min) min = sv[i].tinhTB();
 
-
-            //Bài 2
-            TinhTau tau= new TinhTau();
-            tau.nhapMaDoanTau();
-            tau.tinhTien();
-
-            tau.inMatau();
-            Console.ReadLine();
+            }
+            Console.WriteLine("SV diem thap nhat la: {0} ",min);
+            //tim ra sv co chua chu 18i
+            for (int i = 0; i < n; i++)
+            {
+                if (sv[i].maSV.Contains("IT"))
+                    Console.WriteLine("SV ma {0} co diem tb la: {1} ", sv[i].maSV,sv[i].tinhTB());
+            }
+            //sap xep mang sv theo diem tb tang dan
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (sv[j].tinhTB() < sv[i].tinhTB())
+                    {
+                        sinhvien sxm;
+                        sxm = sv[i];
+                        sv[i] = sv[j];
+                        sv[j] = sxm;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("sinh vien ma {0} co diem tb: {1}\n  ", sv[i].maSV,sv[i].tinhTB());
+            }
+            Console.ReadKey();
 
         }
     }
